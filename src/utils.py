5dc9b13e12b2963060ff78e36cb72c9ff64fb513
michelf1 <michelfa01@buffalostate.edu>
2025-04-18 17:18:24 -0400

    added another column

--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,8 @@ def clean_tree_data(df):
         'Therms Saved',
         'Pollutants Saved (in lbs.)',
         'Property Benefits ($)'
+        'Site ID'
+        #changing some stuff because there is some stuff to be changed
     ]
     df = df[columns_to_keep].dropna().reset_index(drop=True)
     df = df.loc[np.all(df>0,axis=1)].reset_index(drop=True) #on the assumption that zeros are missing data. whateverrrr
