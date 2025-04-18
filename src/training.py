e5ce30f724e7581a9197cdc9711ccaee46fe5562
allie <aferas@anthology.com>
2025-04-18 15:57:37 -0400

    a silly little tutorial

---
+++ src/training.py
@@ -0,0 +1,26 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+from pathlib import Path
+import joblib
+
+
+tree = pd.read_csv('data/processed/clean_trees.csv')
+
+train, test = train_test_split(tree, train_size=0.8, test_size=0.2, random_state=42)
+
+target='Property Benefits ($)'
+xcols = [c for c in train.columns if c != target]
+
+lr = LinearRegression()
+lr.fit(train[xcols],train[target])
+test['prediction'] = lr.predict(test[xcols])
+
+outdir = Path('data/out')
+outdir.parent.mkdir(exist_ok=True, parents=True)
+test.to_csv(outdir.joinpath('predictions.csv'),index=False)
+
+joblib.dump(lr, outdir.joinpath('model.joblib'))
+
+
---
+++ src/utils.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+def clean_tree_data(df):
+    columns_to_keep = [
+        'Stormwater Gallons Saved',
+        'CO2 Avoided (in lbs.)',
+        'kWh Saved',
+        'Therms Saved',
+        'Pollutants Saved (in lbs.)',
+        'Property Benefits ($)'
+    ]
+    df = df[columns_to_keep].dropna().reset_index(drop=True)
+    df = df.loc[np.all(df>0,axis=1)].reset_index(drop=True) #on the assumption that zeros are missing data. whateverrrr
+    return df
